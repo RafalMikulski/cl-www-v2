d4bd63c111a35f150ecf20c172c2efe4e9c66933
RJdev <rjmikulski.dev@gmail.com>
2023-01-11 17:04:09 +0100

    Main navigation is working
    New black horizontal logo
    New fixed menu on mobile & pc
    New jpg for "main header" section
    Rwd

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,42 @@
 const hamburger = document.querySelector(".hamburger");
-const mainNav = document.querySelector(".main-nav-container");
+const mainNav = document.querySelector(".nav-menu");
+const subSzkoleniaSwitch = document.querySelector(".sub-szkolenia-switch");
+const subZabiegiSwitch = document.querySelector(".sub-zabiegi-switch");
+const subMenuSzkolenia = document.querySelector(".sub-menu-szkolenia");
+const subMenuZabiegi = document.querySelector(".sub-menu-zabiegi");
+const arrowSzkolenia = document.querySelector(".arrow-szkolenia");
+const arrowZabiegi = document.querySelector(".arrow-zabiegi");
+const headerTop = document.querySelector(".header-top");
+const logoHamburgerContainer = document.querySelector(
+	".logo-hamburger-container"
+);
 
+/* hamburger menu/submenu open/close */
 hamburger.addEventListener("click", toggleNav);
+
+subSzkoleniaSwitch.addEventListener("click", toggleSubSzkolenia);
+subZabiegiSwitch.addEventListener("click", toggleSubZabiegi);
 
 function toggleNav() {
 	hamburger.classList.toggle("active");
 	mainNav.classList.toggle("active");
+	headerTop.classList.toggle("active");
+	logoHamburgerContainer.classList.toggle("active");
+	subMenuSzkolenia.classList.remove("active");
+	arrowSzkolenia.classList.remove("active");
+	subMenuZabiegi.classList.remove("active");
+	arrowZabiegi.classList.remove("active");
+}
+
+function toggleSubSzkolenia() {
+	subMenuSzkolenia.classList.toggle("active");
+	arrowSzkolenia.classList.toggle("active");
+	subMenuZabiegi.classList.remove("active");
+	arrowZabiegi.classList.remove("active");
+}
+function toggleSubZabiegi() {
+	subMenuZabiegi.classList.toggle("active");
+	arrowZabiegi.classList.toggle("active");
+	subMenuSzkolenia.classList.remove("active");
+	arrowSzkolenia.classList.remove("active");
 }
